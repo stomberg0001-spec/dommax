008fea24b518ce7ec87939bf4bf7b5f0c826f9ba
stomberg0001-spec <stomberg0001-spec@users.noreply.github.com>
2026-04-14 09:13:42 +0500

    feat: structlog JSON renderer для L4 observability
    - configure_logging подключается в lifespan main.py
    - ISO 8601 UTC timestamp, level, logger, stack/exc инфо
    - контекстные поля через structlog.contextvars.merge_contextvars

---
+++ src/logging_config.py
@@ -0,0 +1,46 @@
+"""Структурированное JSON-логирование для L4 observability."""
+
+import logging
+import sys
+
+import structlog
+
+
+def configure_logging(*, debug: bool = False) -> None:
+    """Настроить stdlib logging + structlog в JSON-режиме."""
+    level = logging.DEBUG if debug else logging.INFO
+
+    timestamper = structlog.processors.TimeStamper(fmt="iso", utc=True)
+
+    shared_processors: list = [
+        structlog.contextvars.merge_contextvars,
+        structlog.stdlib.add_logger_name,
+        structlog.stdlib.add_log_level,
+        timestamper,
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+    ]
+
+    structlog.configure(
+        processors=shared_processors + [
+            structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+        ],
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+
+    formatter = structlog.stdlib.ProcessorFormatter(
+        foreign_pre_chain=shared_processors,
+        processors=[
+            structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+            structlog.processors.JSONRenderer(),
+        ],
+    )
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formatter)
+
+    root = logging.getLogger()
+    root.handlers = [handler]
+    root.setLevel(level)
